8c0a261d4990f12088999f00304ee2e95c6dc438
andreamue <89088874+andreamue@users.noreply.github.com>
2023-12-03 03:27:45 -0500

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -1,5 +1,7 @@
+
 #include <iostream>
 #include "vector"
+#include "ufo.h"
 using namespace std;
 
 // Project 3b for COP3530 Ana Garcia Andrea Mueller and Brielle Stokes
@@ -12,33 +14,32 @@ private:
     int numItems = 0;
     int capacity = 100;
     struct hashNode{
+        UFOSighting sighting;
+        int key;
+        string value;
 
-        string key = "";
-        int value = 0;
         hashNode(){
-            key = "";
-            value = 0;
-            
+            key = 0;
+            value = "";
+            sighting = UFOSighting();
+
         }
-        hashNode(string & k, int v){
-            key = k;
-            value = v;
-            
-        }
-        ~hashNode();
+
+        hashNode(UFOSighting sighting) : sighting(sighting), key(sighting.year+sighting.month+sighting.day+sighting.hour+sighting.minute),value(sighting.city+", "+sighting.state){}
 
     };
     vector<vector<hashNode>> hashTable;
-    // location of the sighting and year
-
+    // need to create your own hash map you cant use unordered map
+    // location of the sighting and date
 public:
     Hash(): hashTable(capacity){}
-    int hash(string s){
-        return s.length()%capacity;
+    long hash(int s){
+        return s%capacity;
     }
     void resize(vector<vector<hashNode>>& hashTable) {
         capacity = capacity * 2;
         vector<vector<hashNode>> newTable(capacity);
+
         for (auto& row : hashTable) {
             for (auto& node : row) {
                 int index = hash(node.key);
@@ -47,9 +48,10 @@ public:
         }
         hashTable = newTable;
     }
-    void insert(string name, int year){
+
+    void insert(UFOSighting sighting){
         // create new node to be put in the array "table"
-        hashNode *node = new hashNode(name,year);
+        hashNode *node = new hashNode(sighting);
         //hash function makes index
         int index = hash(node->key);
         // separate chaining: linked list implementation will be better for large data sets
@@ -59,38 +61,58 @@ public:
             //increase table size
             resize(hashTable);
         }
-
     }
-    bool findUFO(string name) {
-        int index = hash(name);
+
+    bool findUFO(int date) {
+        int index = hash(date);
         for (auto &i : hashTable[index]) {
-            if (i.key == name) {
+            if (i.key == date) {
                 return true;
             }
         }
         return false;
     }
-    int getDistance(string location){// NOT IMPLEMENTED
-        int distance;
-        
-        return distance;
-    }
-    int getYear(string sighting){ // EDIT to make the time more specific ( minutes hours days months etc)
-        int ufoYear;
-        int index = hash(sighting);
-        for (auto &i : hashTable[index]) {
-            if (i.key == sighting) {
-               ufoYear = i.value;
+    UFOSighting closestSighting(string city, string state){
+        UFOSighting closest;
+        for(auto &i :hashTable){
+            for(auto &node :i ){
+                if(node.sighting.city == city && node.sighting.state == state){
+                    return node.sighting;
+                }
+                else if(node.sighting.state == state){
+                    closest = node.sighting;
+                }
             }
         }
-        return ufoYear;
+
+        return closest;
     }
-    void print(){
-        for(auto &index : hashTable){
-            for(auto &it : index){
-            cout<<"Sighting location: "<<it.key<<"\nSighting date: "<<it.value<<endl;
+    vector<UFOSighting> mostRecentSighting(){
+        vector<UFOSighting> recents;
+        vector<hashNode> mostRecent = hashTable.back();
+        for( auto &i : mostRecent){
+            // if there is a tie then what
+            recents.push_back(i.sighting);
         }
-        }
+        return recents;
     }
 
+    vector<UFOSighting> ListofSightings(string &state){
+        vector<UFOSighting> sightings;
+        for(auto& i : hashTable){
+            for(auto &node :i){
+                if(node.sighting.state == state){
+                    sightings.push_back(node.sighting);
+                }
+            }
+        }
+        return sightings;
+    }
+    void print(){
+        for(auto&index :hashTable) {
+            for (auto &it: index) {
+                cout << "Sighting location: " << it.key << "\nSighting date: " << it.value << endl;
+            }
+        }
+    }
 };
